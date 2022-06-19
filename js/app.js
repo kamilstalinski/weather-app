50910243df5e0644460055e51a923dbc4bcaf22d
kamilstalinski <kamstal95@gmail.com>
2022-06-19 21:52:33 +0200

    geolocation added

--- js/app.js
+++ js/app.js
@@ -1,1 +1,37 @@
-console.log('działa');
+window.addEventListener('load', () => {
+    let long;
+    let lat;
+
+    const getLocation = () => {
+        if (navigator.geolocation) {
+            navigator.geolocation.getCurrentPosition(position => {
+                long = position.coords.longitude;
+                lat = position.coords.latitude;
+
+
+                let weather = {
+                    apiKey: '1aea2c82b33f33484fe4babbf6bb420f',
+                    fetchCurrentWeather: function () {
+                        fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&appid=${this.apiKey}`)
+                            .then(res => res.json())
+                            .then(data => {
+                                console.log(data)
+                            })
+                    },
+                };
+                weather.fetchCurrentWeather()
+
+            })
+        } else {
+            alert('Geolocation is not supported in this browser')
+        };
+    }
+
+    getLocation();
+
+
+});
+
+
+
+
