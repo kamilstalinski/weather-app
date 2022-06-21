7f198b69bff0410a25af193b1ebc26c545e36a51
kamilstalinski <kamstal95@gmail.com>
2022-06-21 10:21:40 +0200

    improved geolocation position function

--- js/app.js
+++ js/app.js
@@ -1,42 +1,39 @@
-window.addEventListener('load', () => {
-    let long;
-    let lat;
+const getLocation = () => {
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(weather.fetchCurrentWeather);
+    } else {
+        alert('Geolocation is not supported in this browser')
+    };
+}
 
-    const getLocation = () => {
-        if (navigator.geolocation) {
-            navigator.geolocation.getCurrentPosition(position => {
-                long = position.coords.longitude;
-                lat = position.coords.latitude;
+let weather = {
+    fetchCurrentWeather: function (position) {
 
-
-                let weather = {
-                    apiKey: '1aea2c82b33f33484fe4babbf6bb420f',
-                    fetchCurrentWeather: function () {
-                        fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&units=metric&appid=${this.apiKey}`)
-                            .then(res => res.json())
-                            .then(data => this.setCurrentWeather(data))
-                    },
-                    setCurrentWeather: function (data) {
-                        console.log(data)
-                        const { name } = data;
-                        const { temp, humidity } = data.main;
-                        const date = new Date();
-                        document.getElementById('city').innerText = name;
-                        document.getElementById('temp').innerText = Math.ceil(temp) + '°';
-                        document.getElementById('humidity').innerText = humidity;
-                        document.getElementById('date').innerText = `${date.getHours()}:${date.getMinutes()}`;
-                    },
-                };
-                weather.fetchCurrentWeather();
+        let lat = position.coords.latitude;
+        let long = position.coords.longitude;
 
 
-            })
-        } else {
-            alert('Geolocation is not supported in this browser')
-        };
-    }
-    getLocation();
-});
+        fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&units=metric&appid=1aea2c82b33f33484fe4babbf6bb420f`)
+            .then(res => res.json())
+            .then(data => weather.setCurrentWeather(data))
+    },
+    setCurrentWeather: function (data) {
+        // console.log(data)
+        const { name } = data;
+        const { temp, humidity } = data.main;
+        const date = new Date();
+        console.log(date)
+        document.getElementById('city').innerText = name;
+        document.getElementById('temp').innerText = Math.ceil(temp) + '°';
+        document.getElementById('humidity').innerText = humidity;
+        document.getElementById('date').innerText = `${date.getHours()}:${date.getMinutes()}`;
+    },
+};
+
+
+
+
+getLocation();
 
 
 
