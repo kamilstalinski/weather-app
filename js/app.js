4a911f5df42750706d56970da9fa347282ccf41d
kamilstalinski <kamstal95@gmail.com>
2022-06-23 01:25:06 +0200

    added time clock and comments

--- js/app.js
+++ js/app.js
@@ -1,19 +1,41 @@
-
+//Downloading HTML elements
 const weatherCity = document.getElementById('city');
 const weatherTemp = document.getElementById('temp');
 const weatherHumidity = document.getElementById('humidity');
 const weatherDate = document.getElementById('date');
 const weatherDescription = document.getElementById('description');
 
+//Getting current coordinates and running main function
 const getLocation = () => {
     if (navigator.geolocation) {
         navigator.geolocation.getCurrentPosition(currentWeather.fetchCurrentWeather);
+        setClockAndDate();
     } else {
         alert('Geolocation is not supported in this browser')
     };
 };
 
+//setting current Time and Date
+const setClockAndDate = () => {
+    setInterval(() => {
+        const date = new Date();
+
+        let hours = date.getHours();
+        let minutes = date.getMinutes();
+        let seconds = date.getSeconds();
+
+        if (hours < 10) hours = '0' + hours;
+        if (minutes < 10) minutes = '0' + minutes;
+        if (seconds < 10) seconds = '0' + seconds;
+
+        weatherDate.innerText = `${hours}:${minutes}:${seconds}`;
+    }, 1000)
+}
+
+//Object which stores functions about current weather
 let currentWeather = {
+
+    //fetching data from openweathermap.org
     fetchCurrentWeather: function (position) {
 
         let lat = position.coords.latitude;
@@ -25,9 +47,11 @@ let currentWeather = {
             .then(data => currentWeather.setCurrentWeather(data))
     },
 
+    //Setting current weather from API by changing DOM
     setCurrentWeather: function (data) {
         document.querySelector('.loader').style.display = 'none';
         document.querySelector('.weather').classList.add('weather-active');
+        document.querySelector('nav').classList.add('search-active');
         document.querySelector('.additional-info').classList.add('active');
 
         const { name } = data;
@@ -35,17 +59,15 @@ let currentWeather = {
         const { country } = data.sys;
         const { description } = data.weather[0];
 
-        const date = new Date();
-
         weatherCity.innerText = name + ', ' + country;
         weatherTemp.innerText = Math.ceil(temp) + '°';
         weatherHumidity.innerText = humidity + '%';
-        weatherDate.innerText = `${date.getHours()}:${date.getMinutes()}`;
         weatherDescription.innerText = description;
 
         this.setIcon(description);
     },
 
+    //Setting weather icon and background according to current weather 
     setIcon: function (description) {
         const weatherIcon = document.getElementById('icon')
         const bgImage = `linear-gradient(180deg, rgba(68, 78, 88, 0.1) 40%, rgb(0, 0, 0) 100%), url("./img/background/${description}.jpg")`
@@ -88,10 +110,6 @@ let currentWeather = {
         }
     }
 };
-
-
-
-
 
 getLocation();
 
