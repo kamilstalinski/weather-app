8ac94140367403be5a2a4a7720187241b5c1c8e1
kamilstalinski <kamstal95@gmail.com>
2022-06-22 22:44:27 +0200

    added dynamic icons, background, and loader

--- js/app.js
+++ js/app.js
@@ -1,12 +1,19 @@
+
+const weatherCity = document.getElementById('city');
+const weatherTemp = document.getElementById('temp');
+const weatherHumidity = document.getElementById('humidity');
+const weatherDate = document.getElementById('date');
+const weatherDescription = document.getElementById('description');
+
 const getLocation = () => {
     if (navigator.geolocation) {
-        navigator.geolocation.getCurrentPosition(weather.fetchCurrentWeather);
+        navigator.geolocation.getCurrentPosition(currentWeather.fetchCurrentWeather);
     } else {
         alert('Geolocation is not supported in this browser')
     };
-}
+};
 
-let weather = {
+let currentWeather = {
     fetchCurrentWeather: function (position) {
 
         let lat = position.coords.latitude;
@@ -15,20 +22,73 @@ let weather = {
 
         fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&units=metric&appid=1aea2c82b33f33484fe4babbf6bb420f`)
             .then(res => res.json())
-            .then(data => weather.setCurrentWeather(data))
+            .then(data => currentWeather.setCurrentWeather(data))
     },
+
     setCurrentWeather: function (data) {
-        // console.log(data)
+        document.querySelector('.loader').style.display = 'none';
+        document.querySelector('.weather').classList.add('weather-active');
+        document.querySelector('.additional-info').classList.add('active');
+
         const { name } = data;
         const { temp, humidity } = data.main;
+        const { country } = data.sys;
+        const { description } = data.weather[0];
+
         const date = new Date();
-        console.log(date)
-        document.getElementById('city').innerText = name;
-        document.getElementById('temp').innerText = Math.ceil(temp) + '°';
-        document.getElementById('humidity').innerText = humidity;
-        document.getElementById('date').innerText = `${date.getHours()}:${date.getMinutes()}`;
+
+        weatherCity.innerText = name + ', ' + country;
+        weatherTemp.innerText = Math.ceil(temp) + '°';
+        weatherHumidity.innerText = humidity + '%';
+        weatherDate.innerText = `${date.getHours()}:${date.getMinutes()}`;
+        weatherDescription.innerText = description;
+
+        this.setIcon(description);
     },
+
+    setIcon: function (description) {
+        const weatherIcon = document.getElementById('icon')
+        const bgImage = `linear-gradient(180deg, rgba(68, 78, 88, 0.1) 40%, rgb(0, 0, 0) 100%), url("./img/background/${description}.jpg")`
+
+        if (description === 'clear sky') {
+            weatherIcon.setAttribute('src', `img/icons/${description}.svg`);
+            document.body.style.backgroundImage = bgImage;
+        }
+        if (description === 'few clouds') {
+            weatherIcon.setAttribute('src', `img/icons/${description}.svg`);
+            document.body.style.backgroundImage = bgImage;
+        }
+        if (description === 'scattered clouds') {
+            weatherIcon.setAttribute('src', `img/icons/${description}.svg`);
+            document.body.style.backgroundImage = bgImage;
+        }
+        if (description === 'broken clouds') {
+            weatherIcon.setAttribute('src', `img/icons/${description}.svg`);
+            document.body.style.backgroundImage = bgImage;
+        }
+        if (description === 'shower rain') {
+            weatherIcon.setAttribute('src', `img/icons/${description}.svg`);
+            document.body.style.backgroundImage = bgImage;
+        }
+        if (description === 'rain') {
+            weatherIcon.setAttribute('src', `img/icons/${description}.svg`);
+            document.body.style.backgroundImage = bgImage;
+        }
+        if (description === 'thunderstorm') {
+            weatherIcon.setAttribute('src', `img/icons/${description}.svg`);
+            document.body.style.backgroundImage = bgImage;
+        }
+        if (description === 'snow') {
+            weatherIcon.setAttribute('src', `img/icons/${description}.svg`);
+            document.body.style.backgroundImage = bgImage;
+        }
+        if (description === 'mist') {
+            weatherIcon.setAttribute('src', `img/icons/${description}.svg`);
+            document.body.style.backgroundImage = bgImage;
+        }
+    }
 };
+
 
 
 
