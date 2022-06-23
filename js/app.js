4e5b704800381c479294a9603f8c1ea43a79634b
kamilstalinski <kamstal95@gmail.com>
2022-06-23 09:28:48 +0200

    added search possibility for other cities

--- js/app.js
+++ js/app.js
@@ -1,3 +1,5 @@
+const apiKey = '1aea2c82b33f33484fe4babbf6bb420f'
+
 //Downloading HTML elements
 const weatherCity = document.getElementById('city');
 const weatherTemp = document.getElementById('temp');
@@ -6,16 +8,29 @@ const weatherDate = document.getElementById('date');
 const weatherDescription = document.getElementById('description');
 
 //Getting current coordinates and running main function
+let cityName;
+
 const getLocation = () => {
     if (navigator.geolocation) {
-        navigator.geolocation.getCurrentPosition(currentWeather.fetchCurrentWeather);
+        navigator.geolocation.getCurrentPosition(position => {
+            let lat = position.coords.latitude;
+            let long = position.coords.longitude;
+
+            fetch(`http://api.openweathermap.org/geo/1.0/reverse?lat=${lat}&lon=${long}&limit=5&appid=${apiKey}`)
+                .then(res => res.json())
+                .then((data) => {
+                    cityName = data[0].name;
+                    currentWeather.fetchCurrentWeather(cityName)
+                })
+        });
+
         setClockAndDate();
     } else {
         alert('Geolocation is not supported in this browser')
     };
 };
 
-//setting current Time and Date
+// //setting current Time and Date
 const setClockAndDate = () => {
     setInterval(() => {
         const date = new Date();
@@ -32,22 +47,22 @@ const setClockAndDate = () => {
     }, 1000)
 }
 
-//Object which stores functions about current weather
+// //Object which stores functions about current weather
 let currentWeather = {
 
     //fetching data from openweathermap.org
-    fetchCurrentWeather: function (position) {
-
-        let lat = position.coords.latitude;
-        let long = position.coords.longitude;
-
-
-        fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&units=metric&appid=1aea2c82b33f33484fe4babbf6bb420f`)
+    fetchCurrentWeather: function (city) {
+        fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}`)
             .then(res => res.json())
-            .then(data => currentWeather.setCurrentWeather(data))
+            .then(data => this.setCurrentWeather(data))
     },
 
-    //Setting current weather from API by changing DOM
+    searchCity: function () {
+        let input = document.querySelector('.search');
+        this.fetchCurrentWeather(input.value);
+    },
+
+    // Setting current weather from API by changing DOM
     setCurrentWeather: function (data) {
         document.querySelector('.loader').style.display = 'none';
         document.querySelector('.weather').classList.add('weather-active');
@@ -71,6 +86,7 @@ let currentWeather = {
     setIcon: function (description) {
         const weatherIcon = document.getElementById('icon')
         const bgImage = `linear-gradient(180deg, rgba(68, 78, 88, 0.1) 40%, rgb(0, 0, 0) 100%), url("./img/background/${description}.jpg")`
+        console.log(description)
 
         if (description === 'clear sky') {
             weatherIcon.setAttribute('src', `img/icons/${description}.svg`);
@@ -110,6 +126,14 @@ let currentWeather = {
         }
     }
 };
+
+
+
+
+
+document.querySelector('.search-btn').addEventListener('click', () => {
+    currentWeather.searchCity();
+})
 
 getLocation();
 
