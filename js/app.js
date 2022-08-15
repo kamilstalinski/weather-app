3f1f17df0aff3a5057b7c3c3ad90fb0c0ba0fd8e
kamilstalinski <kamstal95@gmail.com>
2022-08-15 21:57:07 +0200

    setting background

--- js/app.js
+++ js/app.js
@@ -1,8 +1,3 @@
-const apiKey = '1aea2c82b33f33484fe4babbf6bb420f';
-const unsplashKey = 'Iu_v5uR8-lAQ0HwMQ8K-9kyQisOyDCZ5qqSUk_Lu1dU';
-const googleApi = '';
-
-
 //Downloading HTML elements
 const weatherCity = document.getElementById('city');
 const weatherTemp = document.getElementById('temp');
@@ -43,6 +38,7 @@ const getLocation = () => {
 
 //Fetching weather data
 const fetchWeatherAPI = (lat, long) => {
+    const apiKey = config.WEATHER_KEY;
 
     Promise.all([
         fetch(`http://api.openweathermap.org/geo/1.0/reverse?lat=${lat}&lon=${long}&limit=5&appid=${apiKey}`)
@@ -217,29 +213,7 @@ const initMap = (lat = 0, long = 0, city = null) => {
 
     const autocomplete = new google.maps.places.Autocomplete(input);
 
-    let request = {};
-
-    if (input.value) {
-        request.query = input.value
-    } else {
-        request.query = city
-    }
-
-    let service = new google.maps.places.PlacesService(map)
-    service.textSearch(request, callback)
-
-
 }
-
-function callback(results, status) {
-    if (status == google.maps.places.PlacesServiceStatus.OK) {
-        let backgroundPhoto = results[0].photos[0].getUrl();
-        const bgImage = `linear-gradient(0deg, rgba(0,0,0,0.8) 30%, rgba(255,255,255,0) 70%),url(${backgroundPhoto})`
-        document.body.style.backgroundImage = bgImage;
-
-    }
-}
-
 
 //Setting current Time and Date
 const setClockAndDate = () => {
@@ -286,9 +260,9 @@ let currentWeather = {
         weatherDescription.innerText = description;
         weatherWind.innerHTML = Math.round(wind_speed) + 'km/h';
         weatherClouds.innerHTML = clouds + '%';
-        // weatherMinTemp.innerHTML =
 
         this.setIcon(description);
+        this.setBackground(description);
     },
 
 
@@ -317,12 +291,17 @@ let currentWeather = {
     },
 
     //Setting weather icon and background according to current weather 
-    setIcon: function (description) {
+    setIcon: (description) => {
         const weatherIcon = document.getElementById('icon')
-
-
         if (description) {
             weatherIcon.setAttribute('src', `img/icons/${description}.svg`);
+        }
+    },
+
+    setBackground: (description) => {
+        if (description) {
+            const bgImage = `linear-gradient(0deg, rgba(0,0,0,0.8) 30%, rgba(255,255,255,0) 70%),url('/img/background/${description}.jpg')`
+            document.body.style.backgroundImage = bgImage;
         }
     }
 };
@@ -336,26 +315,6 @@ let dailyWeather = {
         for (let i = 0; i < dailyData.length - 5; i++) {
             const date = new Date(dailyData[i].dt * 1000);
             const dayName = days[date.getDay()];
-
-            // const dailyContainer = document.querySelector('.next-days');
-            // const detailsContainer = document.createElement('div')
-            // const tempItems = document.createElement('div');
-            // detailsContainer.classList.add('details-container');
-            // const detailItem = document.createElement('div');
-            // detailItem.classList.add('details-item');
-
-            // dailyContainer.appendChild(detailsContainer);
-            // detailsContainer.appendChild(detailItem);
-            // detailItem.innerHTML = `
-            // <div class="temp-items">
-            // <h3>${dayName}</h3>
-            // <p><span id="max">${Math.round(dailyData[i].temp.max)}°</span>/ <span id="min">${Math.round(dailyData[i].temp.min)}°C</span></p>
-            // </div>
-            // <img class="weather-icon" src="./img/icons/scattered clouds.svg" alt="weather-icon">
-            // `
-
-
-
         }
     }
 }
@@ -381,13 +340,4 @@ window.addEventListener('load', () => {
 });
 
 window.initMap = initMap;
-
-
-
-
-
-
-
-
-
 
